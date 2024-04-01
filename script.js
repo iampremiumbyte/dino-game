7246da860a8a9d886eb0e2ce380c4f0106916f47
Godwin Idemudia <godwinide@gmail.com>
2024-04-01 16:06:44 +0100

    new

--- script.js
+++ script.js
@@ -50,9 +50,9 @@ function checkLose() {
 
 function isCollision(rect1, rect2) {
   return (
-    rect1.left < rect2.right &&
-    rect1.top < rect2.bottom &&
-    rect1.right > rect2.left &&
+    (rect1.left + 50) < rect2.right &&
+    (rect1.top + 50) < rect2.bottom &&
+    (rect1.right - 50) > rect2.left &&
     rect1.bottom > rect2.top
   )
 }
